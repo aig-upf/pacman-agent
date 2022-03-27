93710d6a910f611ce5a11ebc27fd6895789ec6a8
js <jsego6@gmail.com>
2022-03-27 20:46:15 +0200

    Template and scripts for building a PACMAN AI agent

---
+++ scripts/config.sh
@@ -0,0 +1,8 @@
+python -m venv venv
+source venv/bin/activate
+git submodule add https://github.com/jsego/pacman-contest.git
+cd pacman-contest/
+pip install -e .
+cd ..
+pip install -r requirements.txt
+# deactivate
